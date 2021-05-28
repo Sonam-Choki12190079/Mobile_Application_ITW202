ec2d18f8833238c1ff1cfb11f8018323bf60e4a1
Sonam-Choki12190079 <s98chokigmail.com>
2021-05-28 15:47:27 +0200

    ToDo_23

---
+++ ToDo_23/app/src/main/java/edu/gcit/todo_23/CustomerReceiver.java
@@ -0,0 +1,32 @@
+package edu.gcit.todo_23;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomerReceiver extends BroadcastReceiver {
+    private static final String ACTION_CUSTOM_BROADCAST= BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String intentAction = intent.getAction();
+        if(intentAction !=null){
+            String toastMessage ="unknown intent action";
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage="Power connected";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage="Power Disconnected";
+                    break;
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage="custom BroadCast Receiver";
+                    break;
+
+            }
+            Toast.makeText(context,toastMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
